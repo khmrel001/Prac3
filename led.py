0d0e65ddf1d6bc2b45084879a8ebeab80e023e69
khmrel001 <khmrel001@github.com>
2020-09-29 11:24:43 +0200

    led.py

--- led.py
+++ led.py
@@ -7,17 +7,27 @@ def main():
     GPIO.setmode(GPIO.BCM)
     
     #assign pin values names
-    LedPin=22
+    ledPin=22
+    pushButton=5
     
     #set up pins
     GPIO.setup(LedPin,GPIO.OUT)
+    GPIO.setup(LedPin,GPIO.IN,pull_up_down=GPIO.PUD_UP)
     
-    #toggle led on and off after 2seconds
+    #toggle led on and off as switch is pressed
     while True:
-        GPIO.output(7,True)
-        time.sleep(2)
-        GPIO.output(7,False)
-        time.sleep(2)
+        #button press
+        buttonPress= GPIO.input(PushButton)      
+        
+        #turn on led if button was pressed and the led was off
+        if buttonPress == False and ledPin==False:
+            GPIO.output(ledPin,True)
+        #turn off led if the button was pressed and the led was on
+        elif buttonPress== False and ledPin==True:
+            GPIO.output(ledPin,False)
+        time.sleep(0.1)
+    
+        
 if name==__name__=="main":
     main()
     
