ab9ecbfd6b86457561d0ff65060e5e141a28aa67
khmrel001 <khmrel001@github.com>
2020-09-29 10:27:46 +0200

    led.py

--- led.py
+++ led.py
@@ -2,14 +2,22 @@ import time
 import RPi.GPIO as GPIO
 
 def main():
-    GPIO.setmode(GPIO.OUT)
-    GPIO.setup(7,GPIO.OUT)
+    
+    #set pin modes
+    GPIO.setmode(GPIO.BCM)
+    
+    #assign pin values names
+    LedPin=22
+    
+    #set up pins
+    GPIO.setup(LedPin,GPIO.OUT)
+    
+    #toggle led on and off after 2seconds
     while True:
         GPIO.output(7,True)
         time.sleep(2)
         GPIO.output(7,False)
         time.sleep(2)
-    
 if name==__name__=="main":
     main()
     
